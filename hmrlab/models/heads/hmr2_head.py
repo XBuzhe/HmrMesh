b75d5968cca60da40b30e4cf74bfeda81eac73a8
xingl <78582472@qq.com>
2024-12-25 10:06:23 +0800

    commit_20241225

--- hmrlab/models/heads/hmr2_head.py
+++ hmrlab/models/heads/hmr2_head.py
@@ -1,37 +1,38 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import einops
 
 from ...utils.geometry import rot6d_to_rotmat, aa_to_rotmat
 from ...networks.pose_transformer import TransformerDecoder
 
-def build_smpl_head(cfg):
-    smpl_head_type = cfg.MODEL.SMPL_HEAD.get('TYPE', 'hmr')
-    if  smpl_head_type == 'transformer_decoder':
-        return SMPLTransformerDecoderHead(cfg)
-    else:
-        raise ValueError('Unknown SMPL head type: {}'.format(smpl_head_type))
 
 class SMPLTransformerDecoderHead(nn.Module):
     """ Cross-attention based SMPL Transformer decoder
     """
 
-    def __init__(self, cfg):
+    def __init__(self):
         super().__init__()
-        self.cfg = cfg
-        self.joint_rep_type = cfg.MODEL.SMPL_HEAD.get('JOINT_REP', '6d')
+        self.joint_rep_type = '6d'
         self.joint_rep_dim = {'6d': 6, 'aa': 3}[self.joint_rep_type]
-        npose = self.joint_rep_dim * (cfg.SMPL.NUM_BODY_JOINTS + 1)
+        npose = self.joint_rep_dim * 24
         self.npose = npose
-        self.input_is_mean_shape = cfg.MODEL.SMPL_HEAD.get('TRANSFORMER_INPUT', 'zero') == 'mean_shape'
+        self.input_is_mean_shape = False
+
         transformer_args = dict(
             num_tokens=1,
             token_dim=(npose + 10 + 3) if self.input_is_mean_shape else 1,
             dim=1024,
+            depth=6,
+            heads=8,
+            mlp_dim=1024,
+            dim_head=64,
+            dropout=0.0,
+            emb_dropout=0.0,
+            norm='layer',
+            context_dim=1280
         )
-        transformer_args = (transformer_args | dict(cfg.MODEL.SMPL_HEAD.TRANSFORMER_DECODER))
+
         self.transformer = TransformerDecoder(
             **transformer_args
         )
@@ -40,13 +41,7 @@ class SMPLTransformerDecoderHead(nn.Module):
         self.decshape = nn.Linear(dim, 10)
         self.deccam = nn.Linear(dim, 3)
 
-        if cfg.MODEL.SMPL_HEAD.get('INIT_DECODER_XAVIER', False):
-            # True by default in MLP. False by default in Transformer
-            nn.init.xavier_uniform_(self.decpose.weight, gain=0.01)
-            nn.init.xavier_uniform_(self.decshape.weight, gain=0.01)
-            nn.init.xavier_uniform_(self.deccam.weight, gain=0.01)
-
-        mean_params = np.load(cfg.SMPL.MEAN_PARAMS)
+        mean_params = np.load('data/hmr2/smpl_mean_params.npz')
         init_body_pose = torch.from_numpy(mean_params['pose'].astype(np.float32)).unsqueeze(0)
         init_betas = torch.from_numpy(mean_params['shape'].astype('float32')).unsqueeze(0)
         init_cam = torch.from_numpy(mean_params['cam'].astype(np.float32)).unsqueeze(0)
@@ -74,7 +69,7 @@ class SMPLTransformerDecoderHead(nn.Module):
         pred_body_pose_list = []
         pred_betas_list = []
         pred_cam_list = []
-        for i in range(self.cfg.MODEL.SMPL_HEAD.get('IEF_ITERS', 1)):
+        for i in range(1):
             # Input token to transformer is zero token
             if self.input_is_mean_shape:
                 token = torch.cat([pred_body_pose, pred_betas, pred_cam], dim=1)[:,None,:]
@@ -103,7 +98,7 @@ class SMPLTransformerDecoderHead(nn.Module):
         pred_smpl_params_list['body_pose'] = torch.cat([joint_conversion_fn(pbp).view(batch_size, -1, 3, 3)[:, 1:, :, :] for pbp in pred_body_pose_list], dim=0)
         pred_smpl_params_list['betas'] = torch.cat(pred_betas_list, dim=0)
         pred_smpl_params_list['cam'] = torch.cat(pred_cam_list, dim=0)
-        pred_body_pose = joint_conversion_fn(pred_body_pose).view(batch_size, self.cfg.SMPL.NUM_BODY_JOINTS+1, 3, 3)
+        pred_body_pose = joint_conversion_fn(pred_body_pose).view(batch_size, 24, 3, 3)
 
         pred_smpl_params = {'global_orient': pred_body_pose[:, [0]],
                             'body_pose': pred_body_pose[:, 1:],
